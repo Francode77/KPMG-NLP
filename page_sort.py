7d108651636ab5bc304f4608e8b2777fcd2bdb31
sedat01 <sedat9404@gmail.com>
2022-12-14 14:05:56 +0100

    bugfix

--- page_sort.py
+++ page_sort.py
@@ -1,5 +1,6 @@
 import os
 from pdfrw import PdfReader, PdfWriter
+from alive_progress import alive_bar
 
 path = "split"
 
@@ -13,15 +14,17 @@ try:
 except:
     pass
 
-for document in os.listdir(path):
-    writer_NL = PdfWriter()
-    writer_FR = PdfWriter()
-    page_number = 0
-    for page in PdfReader(f"{path}/{document}"):
-        page_number += 1
-        if page_number%2 == 1:
-            writer_NL.addpage(page)
-        else:
-            writer_FR.addpage(page)
-    writer_NL.write(f"Nederlands/NL_{document}")
-    writer_FR.write(f"Francais/FR_{document}")
+with alive_bar(len(os.listdir(path))) as bar:
+    for document in os.listdir(path):
+        writer_NL = PdfWriter()
+        writer_FR = PdfWriter()
+        page_number = 0
+        for page in PdfReader(f"{path}/{document}").pages:
+            page_number += 1
+            if page_number%2 == 1:
+                writer_NL.addpage(page)
+            else:
+                writer_FR.addpage(page)
+        writer_NL.write(f"Nederlands/NL_{document}")
+        writer_FR.write(f"Francais/FR_{document}")
+        bar()
