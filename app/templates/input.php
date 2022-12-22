69bb695216ec6c8a07aa7d7baede4d588e255e26
Francode77 <francodefall@gmail.com>
2022-12-22 10:33:46 +0100

    Client application server

---
+++ app/templates/input.php
@@ -0,0 +1,10 @@
+<html>
+   <body>
+   <h2> Output </h2>
+   <h4> The CLA with number<?php echo $CLA; ?></h4>
+      <form action = "http://127.0.0.1/feed_pdf" method = "POST">
+        Provide a PDF File  <input type = "text" name = "PDF" /><br>
+        
+<p><input type = "submit" value = "submit" /></p>
+</form>
+</body>
---
+++ app/templates/output.php
@@ -0,0 +1,6 @@
+
+<html>
+   <body>
+   <h2> Output </h2>
+   <h4> The CLA with number<?php echo $data; ?></h4>
+</body>
